42dff040274679eb52eaef53582cc5986ad6ee8d
Rokibulhasan360 <rokibulhasan360@gmail.com>
2020-05-04 02:23:13 +0600

    index file brought here

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 const express = require('express')
 
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+
 
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express()
 app.use(cors());
 app.use(bodyParser.json());
 
+
+const uri = process.env.DB_PATH;
+
+
+
+
 const users = ["asad","saber","javed","kamal"]
-app.get('/',(req,res)=>{
-    res.send(fruit);
+
+app.get('/products',(req,res) =>{
+  MongoClient.connect(uri, function(err, client) {
+    const collection = client.db("onlineStore").collection("products");
+    collection.find({name : 'Laptop'}).limit(10).toArray((err,documents) =>{
+          if(err){
+            console.log(err)
+          }
+          else{
+            res.send(documents);
+        
+          }
+          
+         })
+    client.close();
+  });
+  
 })
+
+
 
 app.get('/users/:id',(req,res)=>{
     const id = req.params.id;
@@ -20,9 +46,32 @@ app.get('/users/:id',(req,res)=>{
 })
 
 //post
-app.post('/addUser',(req,res) =>{
-   console.log(req.body); 
-})
+app.post('/addProduct',(req,res)=>{
+  const product = req.body;
+ 
+
+  MongoClient.connect(uri, function(err, client) {
+    const collection = client.db("onlineStore").collection("products");
+    collection.insertOne(product,(err,result) =>{
+          if(err){
+            console.log(err)
+          }
+          else{
+            res.send(result.ops[0]);
+          //  res.status(500).send({message:err});
+          }
+          
+         })
+    client.close();
+  });
+  
+
+  
+});
 
 
-app.listen(3400,()=>console.log('listening to port 3400'));
+
+
+const port = process.env.PORT || 3500;
+
+app.listen(port,()=>console.log('listening to port 3500'));
